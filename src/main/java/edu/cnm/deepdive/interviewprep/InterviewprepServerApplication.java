1cfb72ea82c2e8756fde8bfd0c30a7758de615ba
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-27 15:13:57 -0400

    Initial Commmit

---
+++ src/main/java/edu/cnm/deepdive/interviewprep/InterviewprepServerApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.interviewprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewprepServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InterviewprepServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/interviewprep/InterviewprepServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.interviewprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewprepServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

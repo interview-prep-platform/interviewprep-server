e87a83d54c0edbd62ce64cd4e3a3b3daa2b34ac8
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-16 12:06:17 -0500

    Created ExceptionResponseMapping

---
+++ src/main/java/edu/cnm/deepdive/interviewprep/controller/ExceptionResponseMapping.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.interviewprep.controller;
+
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionResponseMapping {
+
+  @ExceptionHandler(NoSuchElementException.class) //Nosuch element exception with associated with this status
+  @ResponseStatus(value = HttpStatus.NOT_FOUND, reason="Resource not found")
+  public void notFound() {
+  }
+
+
+
+}

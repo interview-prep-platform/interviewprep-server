798f47f85db60a0d6d8b6ec77536b5c7e71d60e1
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-16 11:41:04 -0500

    Updated delete method of HistoryController and QuestionController.

--- src/main/java/edu/cnm/deepdive/interviewprep/controller/HistoryController.java
+++ src/main/java/edu/cnm/deepdive/interviewprep/controller/HistoryController.java
@@ -6,12 +6,15 @@ import edu.cnm.deepdive.interviewprep.service.HistoryService;
 import edu.cnm.deepdive.interviewprep.service.UserService;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,6 +29,12 @@ public class HistoryController {
       HistoryService historyService) {
     this.userService = userService;
     this.historyService = historyService;
+  }
+
+  @DeleteMapping(value = "/{externalKey}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable UUID externalKey) {
+    historyService.delete(externalKey);
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/edu/cnm/deepdive/interviewprep/controller/QuestionController.java
+++ src/main/java/edu/cnm/deepdive/interviewprep/controller/QuestionController.java
@@ -5,6 +5,7 @@ import edu.cnm.deepdive.interviewprep.service.QuestionService;
 import edu.cnm.deepdive.interviewprep.service.UserService;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -48,6 +50,7 @@ public class QuestionController {
   }
 
   @DeleteMapping(value = "/{externalKey}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable UUID externalKey) {
     questionService.delete(externalKey);
   }
